9e9668d0f25fb6323bc874783941170cd1220131
manujelly <47712129+manujelly@users.noreply.github.com>
2019-10-12 12:36:53 +0200

    Update and rename supjeans_282997.sql to db.sql

--- db.sql
+++ db.sql
@@ -41,13 +41,7 @@ CREATE TABLE IF NOT EXISTS `products` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=127 DEFAULT CHARSET=utf8;
 
---
--- Déchargement des données de la table `products`
---
 
-INSERT INTO `products` (`id`, `receipt_ref`, `product_name`, `price`, `user`, `transaction_date`, `billing_address`, `delivery_address`) VALUES
-(126, '453693433', 'First shoes kids product', 5, 'angemanou99@gmail.com', '2019-05-02 12:50:12', '4 rÃ©sidence du parc de petit bourg 91000', '4 rÃ©sidence du parc de petit bourg'),
-(125, '453693433', 'First clothing kids product', 42, 'angemanou99@gmail.com', '2019-05-02 12:50:12', '4 rÃ©sidence du parc de petit bourg 91000', '4 rÃ©sidence du parc de petit bourg');
 
 -- --------------------------------------------------------
 
@@ -66,14 +60,8 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
---
--- Déchargement des données de la table `user`
---
 
-INSERT INTO `user` (`id`, `email`, `billing_address`, `delivery_address`, `password`, `status`) VALUES
-(8, 'angemanou99@gmail.com', '4 rÃ©sidence du parc de petit bourg 91000', '4 rÃ©sidence du parc de petit bourg', '768957a081cf27da9c4ddab74aadc7d2', 'admin'),
-(9, '282997@supinfo.com', NULL, NULL, '768957a081cf27da9c4ddab74aadc7d2', 'admin');
-COMMIT;
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
